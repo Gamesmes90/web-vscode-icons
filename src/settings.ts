2b5c372beab889138f6884a156a902c3e9ec6f83
Gamesmes90 <gamesmes90@gmail.com>
2022-04-19 23:27:48 +0200

    TODO: add custom urls

--- src/settings.ts
+++ src/settings.ts
@@ -6,43 +6,45 @@
 //     document.body.appendChild(x);
 // }
 
-function saveOptions(e: { preventDefault: () => void; }) {
-    e.preventDefault();
-    chrome.storage.sync.set({
-      color: (document.querySelector("#color") as HTMLInputElement).value 
-    });
-  }
+// function saveOptions(e: { preventDefault: () => void; }) {
+//     e.preventDefault();
+//     chrome.storage.sync.set({
+//       color: (document.querySelector("#color") as HTMLInputElement).value 
+//     });
+//   }
   
-  function restoreOptions() {
+//   function restoreOptions() {
   
-    function setCurrentChoice(result: { color: string; }) {
-        (document.querySelector("#color") as HTMLInputElement) || "blue";
-    }
+//     function setCurrentChoice(result: { color: string; }) {
+//         (document.querySelector("#color") as HTMLInputElement) || "blue";
+//     }
   
-    function onError(error: any) {
-      console.log(`Error: ${error}`);
-    }
+//     function onError(error: any) {
+//       console.log(`Error: ${error}`);
+//     }
   
-    let getting = chrome.storage.sync.get("color");
-    getting.then(setCurrentChoice, onError);
-  }
+//     let getting = chrome.storage.sync.get("color");
+//     getting.then(setCurrentChoice, onError);
+//   }
   
-  document.addEventListener("DOMContentLoaded", restoreOptions);
-  document.querySelector("form").addEventListener("submit", saveOptions);
+//   document.addEventListener("DOMContentLoaded", restoreOptions);
+//   document.querySelector("form").addEventListener("submit", saveOptions);
   
 
-  function onError(error: any) {
-    console.log(`Error: ${error}`);
-  }
+//   function onError(error: any) {
+//     console.log(`Error: ${error}`);
+//   }
   
-  function onGot(item: { color: string; }) {
-    let color = "blue";
-    if (item.color) {
-      color = item.color;
-    }
-    document.body.style.border = "10px solid " + color;
-  }
+//   function onGot(item: { color: string; }) {
+//     let color = "blue";
+//     if (item.color) {
+//       color = item.color;
+//     }
+//     document.body.style.border = "10px solid " + color;
+//   }
   
-  let getting = chrome.storage.sync.get("color");
-  getting.then(onGot, onError);
+//   let getting = chrome.storage.sync.get("color");
+//   getting.then(onGot, onError);
   
+  console.log("test");
+  //chrome.runtime.getManifest().content_scripts[3].matches = chrome.runtime.getManifest().content_scripts[3].matches.concat();
