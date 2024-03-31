6a43183f3e3eee7814540d9002c35e65ba4b56aa
Gamesmes90 <gamesmes90@gmail.com>
2024-03-31 05:27:00 +0200

    Github overhaul

--- src/GitHub.ts
+++ src/GitHub.ts
@@ -2,109 +2,228 @@ import { getIconForFile, getIconForFolder, getIconForOpenFolder, DEFAULT_FILE, D
 import { isRepoRoot, isSingleFile, isRepoTree, isRepo, utils } from 'github-url-detection';
 import { observe } from 'selector-observer';
 
-export const QUERY_FILE_TABLE_ITEMS = 'div.js-navigation-container>div.js-navigation-item';
-export const QUERY_PATH_SEGMENTS = '.repository-content .js-path-segment a';
-export const QUERY_PJAX_CONTAINER = 'main';
-export const QUERY_LAST_PATH_SEGMENT = '.final-path';
+export const ROOT_TABLE = ".Box-sc-g0xbh4-0";
+export const ROOT_SIDE_TREE = "ul.TreeView__UlBox-sc-4ex6b6-0";
+export const BACK_FOLDER = 'tr.Box-sc-g0xbh4-0';
+export const TREE_ITEM = 'tr.react-directory-row';
+export const SIDE_TREE_FILE = 'div.PRIVATE_TreeView-item-content';
+export const SIDE_TREE_OPEN_FOLDER = 'svg.octicon-file-directory-open-fill';
+export const SIDE_TREE_FOLDER = 'svg.octicon-file-directory-fill';
+
+const OPEN_FOLDER_CLASS = SIDE_TREE_OPEN_FOLDER.split('.')[1];
+const SIDE_TREE_FOLDER_CLASS = SIDE_TREE_FOLDER.split('.')[1];
 
 const isHistoryForFile = () => isRepo() && /^\/commits\/[0-9a-f]{5,40}\/.+/.test(utils.getCleanPathname());
 const getIconUrl = (iconFileName: string) => chrome.runtime.getURL('icons/' + iconFileName);
-/**
- * Show icon for path segments
- */
 
-function showIconsForSegments () {
-  if (!((!isRepoRoot() && isRepoTree()) || isSingleFile() || isHistoryForFile())) return;
-  const aSegments = document.querySelectorAll<HTMLAnchorElement>(QUERY_PATH_SEGMENTS);
-  const firstSegment = aSegments[0];
-  const finalSegment = document.querySelector(QUERY_LAST_PATH_SEGMENT) as HTMLSpanElement | undefined;
-  
-  // first segment has always root folder icon
-  if (firstSegment) {
-    const spanEl = firstSegment.children[0] as HTMLSpanElement;
-    spanEl.innerHTML = `<img src="${getIconUrl(DEFAULT_ROOT_OPENED)}" alt="icon" width="16" height="16"><span> ${
-      spanEl.innerText
-    }</span>`;
-  }
-  
-  // check if final segment is file or folder
-  if (finalSegment) { //String must be trimmed as there might be whitespaces at the start causing the retrieval of the wrong icon
-    const iconPath = window.location.href.includes('/blob/')
-      ? getIconForFile(finalSegment.innerText.trim().toLowerCase()) 
-      : getIconForOpenFolder(finalSegment.innerText.trim().toLowerCase());
-    finalSegment.innerHTML = `<img src="${getIconUrl(iconPath)}" alt="icon" width="16" height="16"><span> ${
-      finalSegment.innerText
-    }</span>`;
-  }
-  
-  // segments between first and last are always folders
-  for (let i = 1; i < aSegments.length; i++) {
-    const spanEl = aSegments[i];
-    const aEl = spanEl.firstChild as HTMLAnchorElement;
-    const iconPath = getIconForOpenFolder(aEl.innerText.trim().toLowerCase());
-    aEl.innerHTML = `<img src="${getIconUrl(iconPath)}" alt="icon" width="16" height="16"><span> ${aEl.innerText}</span>`;
-  }
-  }
-  
-  /**
-   * Show icons for repository files
-   */
-function showRepoTreeIcons(rowEl: Element) {
-  const iconEl = rowEl.children[0] as HTMLTableCellElement;
-  const iconSVGEl = iconEl.querySelector<SVGElement>('.octicon');
-  if (!iconSVGEl) {
-    // ... (up)
+/**
+ * Show icon for folder tree
+ * @param svg SVGElement
+ * @returns 
+ */
+function showFolderIconTree(svg : SVGElement){
+  if(svg.previousElementSibling){
+    svg.remove();
     return;
   }
-  /**
-   * <div role="row">
-   *  <div><svg class={{icon}}/></div>,
-   *  <div><span><a>{{name}}</a></span></div>,
-   *  <div><span><a>{{message}}</a></span></div>,
-   *  <div><span>{time}</span><s/div>,
-   * </div>
-   */
-  const contentEl = rowEl.children[1] as Element;
 
-  const linkToEl = contentEl.firstElementChild.firstElementChild as HTMLAnchorElement;
+  const name = svg.parentElement.parentElement.parentElement.querySelector('span > span')?.textContent?.toLowerCase();
+  if (!name) return;
 
-  let iconPath = '';
-  if (iconSVGEl) {
-    const iconSVGClassName = iconSVGEl.className.baseVal;
-    if (iconSVGClassName.includes('octicon-file-text') || iconSVGClassName.includes('octicon-file ')) {
-      iconPath = getIconForFile(linkToEl.innerText.toLowerCase()); // IconFile
-    } else if (iconSVGClassName.includes('octicon-file-directory')) {
-      const name = linkToEl.innerText.toLowerCase();
-      iconPath = getIconForFolder(name.split('/').shift()); // IconFolder
-    } else if (iconSVGClassName.includes('octicon-file-submodule')) {
-      iconPath = getIconForFolder('submodules');
-    } else if (iconSVGClassName.includes('octicon-file-symlink-file')) {
-      iconPath = DEFAULT_FILE;
-    } else if (iconSVGClassName.includes('octicon-file-symlink-directory')) {
-      iconPath = DEFAULT_FILE;
-    } else {
-      console.error(chrome.runtime.getManifest().name + ": " + `Unknown filetype: "${iconSVGClassName}", please report`);
-      return;
+  const newIconEl = document.createElement('img');
+  newIconEl.setAttribute('src', getIconUrl(getIconForFolder(name)));
+  newIconEl.setAttribute('class', 'web-vscode-icon');
+  newIconEl.setAttribute('width', '16');
+  newIconEl.setAttribute('height', '16');
+
+  svg.parentNode.replaceChild(newIconEl, svg);
+}
+
+/**
+ * Show icon for file tree
+ * @param div HTMLDivElement
+ * @returns 
+ */
+function showIconForFileTree(div : HTMLDivElement){
+  const svg = div.querySelector('div > svg');
+  if (!svg) return;
+  // Skip if it's a folder
+  if (svg.classList.contains(OPEN_FOLDER_CLASS) || svg.classList.contains(SIDE_TREE_FOLDER_CLASS)) return;
+
+  const name = div.querySelector('span > span')?.textContent?.toLowerCase();
+  if (!name) return;
+
+  const newIconEl = document.createElement('img');
+  newIconEl.setAttribute('src', getIconUrl(getIconForFile(name)));
+  newIconEl.setAttribute('class', 'web-vscode-icon');
+  newIconEl.setAttribute('width', '16');
+  newIconEl.setAttribute('height', '16');
+
+  svg.parentNode.replaceChild(newIconEl, svg);
+}
+/**
+ * Show icon for side tree
+ */
+function showIconsForSideTree (tree : HTMLUListElement) {
+  if(isRepoRoot()) return;
+
+  /* Structure of a tree item: Folder
+    <li>
+      <div>
+        <div>
+          <div>
+            <div>
+              <svg></svg>
+            </div>
+          </div>
+          <span>
+            <span></span>
+          </span>
+        </div>
+      </div>
+      <ul>
+        ... recursive
+      </ul>
+    </li>
+  */
+  /* Structure of a tree item: File (One less div)
+    <li>
+      <div>
+        <div>
+            <div>
+              <svg></svg>
+            </div>
+          <span>
+            <span></span>
+          </span>
+        </div>
+      </div>
+      <ul>
+        ... recursive
+      </ul>
+    </li>
+  */
+
+  observe(SIDE_TREE_FILE, {
+    add(div) {
+      showIconForFileTree(div as HTMLDivElement);
     }
-    
-    iconSVGEl.outerHTML = `<img src="${getIconUrl(iconPath)}" class="web-vscode-icon ${iconSVGClassName}" alt="icon" width="16" height="16">`;
-
-  }
-  // else {
-  //   console.error(`Error during parsing: "td.icon > svg.octoicon" doesnt exists for ${i}. row`);
-  // }
-}
-
-function init(){
-  const elements = document.querySelectorAll("div.Box-row.Box-row--focus-gray.py-2.d-flex.position-relative.js-navigation-item");
-
-  observe(QUERY_FILE_TABLE_ITEMS, {
-    add(rowEl) {
-      showRepoTreeIcons(rowEl);
-    },
   });
-  showIconsForSegments();
+
+  observe(SIDE_TREE_OPEN_FOLDER, {
+    add(svg){
+      showFolderIconTree(svg as SVGElement);
+    }
+  });
+
+  observe(SIDE_TREE_FOLDER, {
+    add(svg){
+      showFolderIconTree(svg as SVGElement);
+    }
+  });
+}
+  
+/**
+ * Shows icon for single element
+ */
+function showIconForSingleElement(tr : HTMLTableRowElement) {
+  const svg = tr.querySelector('td > div > svg');
+  if (!svg) return;
+
+  const a = tr.querySelector('td > div > div > h3 > div > a');
+  if (!a) return;
+
+  const name = a.getAttribute('title')?.toLowerCase();
+  if (!name) return;
+
+  const newIconEl = document.createElement('img');
+  if(a.getAttribute('aria-label').includes('File'))
+    newIconEl.setAttribute('src', getIconUrl(getIconForFile(name)));
+  else
+    newIconEl.setAttribute('src', getIconUrl(getIconForFolder(name)));
+  newIconEl.setAttribute('class', 'web-vscode-icon');
+  newIconEl.setAttribute('width', '16');
+  newIconEl.setAttribute('height', '16');
+
+  svg.parentNode.replaceChild(newIconEl, svg);
 }
 
-init();
-document.addEventListener('pjax:end', init); // Update on page change
+/**
+ * Shows Icon for the back button in the tree
+ * @param tr table row element
+ * @see showIconForSingleElement
+ */
+function showIconForBackButton(tr : HTMLTableRowElement) {
+  const svg = tr.querySelector('td > a > div > svg');
+  if (!svg) return;
+  const newIconEl = document.createElement('img');
+  newIconEl.setAttribute('src', getIconUrl(getIconForFolder('..')));
+  newIconEl.setAttribute('class', 'web-vscode-icon');
+  newIconEl.setAttribute('width', '16');
+  newIconEl.setAttribute('height', '16');
+
+  svg.parentNode.replaceChild(newIconEl, svg);
+}
+
+/**
+ * Show icons for repository files
+ */
+function showRepoTreeIcons(table: HTMLTableElement) {
+  /* Each row in the table has the following structure:
+    <table>
+      <tbody>
+        <tr>
+          <td class="...small...">
+            <div>
+              <svg></svg>
+              <div>
+                <h3>
+                  <div>
+                    <a></a>
+                  </div>
+                </h3>
+              </div>
+            </div>
+          </td>
+          <td class="...large...">
+            <div>
+              <svg></svg>
+              <div>
+                <h3>
+                  <div>
+                    <a></a>
+                  </div>
+                </h3>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  */
+
+  //Show icon for each row
+  observe(TREE_ITEM, {
+    add(tr) {
+      showIconForSingleElement(tr as HTMLTableRowElement);
+    }
+  });
+
+  if(!isRepoRoot()) //Show back button icon
+    observe(BACK_FOLDER, {
+      add(tr) {
+        showIconForBackButton(tr as HTMLTableRowElement);
+      }
+    });
+}
+
+observe(ROOT_TABLE, {
+  add(table) {
+    showRepoTreeIcons(table as HTMLTableElement);
+  }
+});
+observe(ROOT_SIDE_TREE, {
+  add(tree) {
+    showIconsForSideTree(tree as HTMLUListElement);
+  }
+});
